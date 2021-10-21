d4c3fa4d8e2484443180e608a8ef1fadabace52a
pkkulhari <praveen.kulhari07@gmail.com>
2021-10-21 23:20:21 +0530

    test: switch to using unittest for fts, extends with user story

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,26 @@
+from unittest import TestCase
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'worked successfully' in browser.title
+
+class NewUserTest(TestCase):
+    """
+    Test for a new user actions 
+    """
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_starting_a_new_todo_list(self):
+        """
+        Test for when a user creates a new todo list
+        """
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
