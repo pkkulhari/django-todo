2a0fa183a7e185539627497420fac7b700725ff9
pkkulhari <praveen.kulhari07@gmail.com>
2021-10-24 09:23:21 +0530

    feat: extend home_page view by adding a feature to handle post requests and pass all todo-items as context in get request

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from lists.models import Item
 
 
 def home_page(request):
-    return render(request, 'home.html')
+
+    if request.method == 'POST':
+        todoItem = request.POST['todo-item']
+        Item.objects.create(body=todoItem)
+        return redirect('lists:home')
+
+    todoItems = Item.objects.all()
+
+    context = {'todoItems': todoItems}
+    return render(request, 'home.html', context)
