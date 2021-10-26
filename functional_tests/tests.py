9b3817e651308d4a94e9ceace2192ca5077705ba
pkkulhari <praveen.kulhari07@gmail.com>
2021-10-26 18:18:01 +0530

    test: remove implicitly wait

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -14,7 +14,6 @@ class NewUserTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(10)
 
     def tearDown(self):
         self.browser.quit()
