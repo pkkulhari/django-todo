31947286efce4269503e11a601901bec8c549f2e
pkkulhari <praveen.kulhari07@gmail.com>
2021-10-26 19:35:04 +0530

    fix: remove the code to create a new todo list from home page

--- lists/views.py
+++ lists/views.py
@@ -3,13 +3,6 @@ from lists.models import Item, TodoList
 
 
 def home_page(request):
-
-    if request.method == 'POST':
-        todoItem = request.POST['todo-item']
-        _list = TodoList.objects.create()
-        Item.objects.create(body=todoItem, list=_list)
-        return redirect('lists:lists')
-
     todoItems = Item.objects.all()
 
     context = {'todoItems': todoItems}
