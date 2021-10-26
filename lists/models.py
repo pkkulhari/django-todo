7fd2254820787093be40c6ed61f614657595b0fa
pkkulhari <praveen.kulhari07@gmail.com>
2021-10-26 18:22:09 +0530

    feat: create new model TodoList and add a list filed into Item model refrencing to it

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 
+class TodoList(models.Model):
+    pass
+
+
 class Item(models.Model):
     body = models.TextField()
+    list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.body
