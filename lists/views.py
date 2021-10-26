7749871f7f05f0a8d3fb37d7e357531dd011c42c
pkkulhari <praveen.kulhari07@gmail.com>
2021-10-26 18:27:11 +0530

    feat: extend home_page view by adding List model refrence and updating redirection url of post request

--- lists/views.py
+++ lists/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, TodoList
 
 
 def home_page(request):
 
     if request.method == 'POST':
         todoItem = request.POST['todo-item']
-        Item.objects.create(body=todoItem)
-        return redirect('lists:home')
+        _list = TodoList.objects.create()
+        Item.objects.create(body=todoItem, list=_list)
+        return redirect('lists:lists')
 
     todoItems = Item.objects.all()
 
